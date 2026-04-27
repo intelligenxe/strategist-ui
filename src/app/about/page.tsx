d8ccf8e58f19da6ea90949f9521e4c138a8ea535
intelligenxe-admin <intelligenxe@gmail.com>
2026-04-26 18:18:09 -0600

    feat: enhance clarity in About component by refining text and improving structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,26 +14,35 @@ export default function About() {
           </a>
           ’s Strategist
         </strong>{" "}
-        uses
+        platform uses
         AI (Artificial Intelligence) to design corporate strategies for
         publicly traded companies worldwide.
       </p>
       <p className="text-gray-600 mb-4">
         <strong className="font-bold">
-          For institutional investors and investment bankers:
+          For institutional investors (including mutual fund and VC fund managers) and investment bankers:
         </strong>{" "}
-        Strategist enables you to assess the gap between human-designed and
-        AI-generated corporate strategies across your companies and industries
-        of interest.
+        Strategist enables them to assess the gaps between human-designed (current) and AI-generated
+        corporate strategies across their companies and industries of interest.
       </p>
       <p className="text-gray-600 mb-4">
-        <strong className="font-bold">For C-suite executives:</strong>{" "}
-        Strategist helps you validate and uncover corporate strategies that can
-        position your organization to thrive in its industry.
+        <strong className="font-bold">For C-suite executives and experienced entrepreneurs:</strong>{" "}
+        Strategist helps them validate and uncover corporate strategies that can position their
+        organizations to thrive in their industries.
       </p>
       <p className="text-gray-600">
-        Strategist may also be valuable to individual investors and
-        entrepreneurs in similar ways.
+        INTELLIGENXE invites the above professionals to test the Strategist's
+        <strong className="font-bold"> experimental version</strong> offered on this website. Accepting this invitation
+        automatically <strong className="font-bold">binds</strong> users to
+        <a
+          href="https://intelligenxe.com/?page_id=177"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          {" "}INTELLIGENXE's Terms & Conditions | User Agreement{" "}
+        </a>
+        .
       </p>
     </div>
   );
